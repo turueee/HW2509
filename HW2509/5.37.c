98cb2caa46d0065ecd32eaadce667f707ac5d63b
turueee <turushkine@inbox.ru>
2024-09-24 23:02:51 +0300

    'HW'

--- HW2509/5.37.c
+++ HW2509/5.37.c
@@ -5,14 +5,21 @@
 
 int main()
 {
-  int i = 1, x = 2, ch = 1, z = 2;
-  double res = 1;
+  int i = 1,chc = 1;
+  double res = 1, x = 2, ch = 1, z = 2,u = 1;
   for (i; i <= 10; i++)
   {
-    if ((ch + i) % 2 == 0)
-      res += ((ch + i) / (z + i) * (pow(x, i)));
+    if ((chc + i) % 2 == 0)
+    {
+      res -= ((ch + u) / (z + u) * (pow(x, u)));
+      printf("%lf %lf %lf %d\n",ch+u, z+u, u, 1);
+    }
     else
-      res -= ((ch + i) / (z + i) * (pow(x, i)));
+    {
+      res += ((ch + u) / (z + u) * (pow(x, u)));
+      printf("%lf %lf %lf %d\n", ch+u, z+u, u,0);
+    }
+    u++;
   }
   printf("%lf\n", res);
 }
